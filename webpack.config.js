103bc823e96034c370b4cf2234f96736bbf04f8a
Artur Pryka <artur.pryka@keen.io>
2019-12-11 09:43:45 +0100

    chore(integrate prettier): integrate prettier and eslint, export getStyle function
    #40

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const extendedPath = path.resolve(__dirname, 'dist');
 const fileName = process.env.BUILD_MODE;
-const entry = ( process.env.BUILD_MODE === 'standalone' ) ? './test/demo/example/index.js' : './lib/components/Builder.js';
+const entry =
+  process.env.BUILD_MODE === 'standalone'
+    ? './test/demo/example/index.js'
+    : './lib/index.js';
 
 module.exports = {
   entry,
@@ -13,66 +16,60 @@ module.exports = {
 
   output: {
     path: extendedPath,
-    filename: `${
-      fileName
-    }.js`,
+    filename: `${fileName}.js`,
     library: `${!process.env.LIBRARY ? '' : process.env.LIBRARY}`,
-    libraryTarget: 'umd',
+    libraryTarget: 'umd'
   },
 
   module: {
-
     rules: [
       {
         test: /\.js?$/,
-        include: [
-          path.resolve(__dirname, ''),
-        ],
-        exclude: [
-          path.resolve(__dirname, 'node_modules'),
-        ],
-        loader: 'babel-loader',
+        include: [path.resolve(__dirname, '')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'babel-loader'
       },
 
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: 'html-loader'
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader',
+        use: [
+          'style-loader',
+          'css-loader',
           {
             loader: 'postcss-loader',
             options: {
               config: {
                 path: __dirname + '/postcss.config.js'
               }
-            },
-          },
-        ],
+            }
+          }
+        ]
       },
       {
         test: /\.svg$/,
-        use: ['@svgr/webpack'],
+        use: ['@svgr/webpack']
       }
-    ],
-
+    ]
   },
 
-  plugins: ( process.env.BUILD_MODE === 'standalone' ) ? 
-    [
-      new HtmlWebPackPlugin({
-        template: './test/demo/index.html',
-        filename: './index.html',
-        title: 'Theme Builder'
-      })
-    ] : [],
+  plugins:
+    process.env.BUILD_MODE === 'standalone'
+      ? [
+          new HtmlWebPackPlugin({
+            template: './test/demo/index.html',
+            filename: './index.html',
+            title: 'Theme Builder'
+          })
+        ]
+      : [],
 
   resolve: {
-    modules: [
-      'node_modules',
-    ],
-    extensions: ['.js', '.json', '.jsx'],
+    modules: ['node_modules'],
+    extensions: ['.js', '.json', '.jsx']
   },
 
   optimization: {},
@@ -89,21 +86,23 @@ module.exports = {
     contentBase: path.join(__dirname, 'test/demo'),
     publicPath: '/',
     open: true,
-    watchContentBase: true,
+    watchContentBase: true
   },
 
-  externals: process.env.BUILD_MODE !== 'standalone' ? {
-    'common-tags' : true,
-    'highlight.js' : true,
-    'keen-react-charts' : true,
-    'prop-types' : true,
-    'react' : true,
-    'react-color' : true,
-    'react-dom' : true,
-    'react-lowlight' : true,
-    'react-select': true,
-    'react-tabs' : true,
-    'styled-jsx' : true,
-    } : {
-  },
+  externals:
+    process.env.BUILD_MODE !== 'standalone'
+      ? {
+          'common-tags': true,
+          'highlight.js': true,
+          'keen-react-charts': true,
+          'prop-types': true,
+          react: true,
+          'react-color': true,
+          'react-dom': true,
+          'react-lowlight': true,
+          'react-select': true,
+          'react-tabs': true,
+          'styled-jsx': true
+        }
+      : {}
 };
